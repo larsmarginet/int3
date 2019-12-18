5ae9bc16a0bfc1acbe6599d97f7ef992b0a875db
larsmarginet <lars.marginet@student.howest.be>
2019-12-18 19:54:01 +0100

    test met GPS en API (GPS bord werkt niet, dus hardcoded coordinaten)

--- Physique/src/renderer/index.js
+++ Physique/src/renderer/index.js
@@ -1,33 +1,62 @@
-const { Board, Led } = require("johnny-five");
-const board = new Board({
-  repl: false
-});
+// "id": 2794056,
+// "name": "Kortrijk",
+// "country": "BE",
+// "coord": {
+//   "lon": 3.26459,
+//   "lat": 50.82811
 
-board.on("ready", () => {
-  // Initialize the RGB LED
-  const led = new Led.RGB({
-    pins: {
-      red: 6,
-      green: 5,
-      blue: 3
+const { Board, GPS } = require("johnny-five");
+let gpsData = {}
+
+const getGpsData = () => {
+  const board = new Board({
+    repl: false
+  });
+  
+  board.on("ready", () => {
+    const gps = new GPS({
+      pins: {
+        rx: 11,
+        tx: 10,
+      }
+    });
+  
+    // If latitude, longitude data log it.
+    // This will output zero until a valid
+    // GPS position is detected.
+    gps.on("data", position => {
+      const {altitude, latitude, longitude} = position;
+      console.log("GPS Position:");
+      console.log("  altitude   : ", altitude);
+      console.log("  latitude   : ", latitude);
+      console.log("  longitude  : ", longitude);
+      console.log("--------------------------------------");
+    });
+    gpsData = {
+      lat: gps.latitude,
+      long: gps.longitude
     }
   });
+};
 
-  // RGB LED alternate constructor
-  // This will normalize an array of pins in [r, g, b]
-  // order to an object (like above) that's shaped like:
-  // {
-  //   red: r,
-  //   green: g,
-  //   blue: b
-  // }
-  // const led = new Led.RGB([3,5,6]);
+const getWeather = (lon, lat) => {
+  var key = '6754a08c79ebc94732217c11986c0de8';
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    console.log(data);
+    console.log(data.list[0]);
+    document.querySelector('.temp').textContent = `${data.list[2].dt_txt}: ${Math.floor(data.list[2].main.temp - 273.15)}°C`;
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
 
-  // Add led to REPL (optional)
+const init = () => {
+  getGpsData();
+  console.log(gpsData);
+  getWeather(3.26, 50.83);
+}
 
-  // Turn it on and set the initial color
-  led.on();
-  led.color("#FF0000");
-
-  led.blink(1000);
-});
+init();
