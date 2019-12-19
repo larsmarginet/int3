1118ca0e12660919f913a425c66c1c4325e73dfd
larsmarginet <lars.marginet@student.howest.be>
2019-12-19 16:36:25 +0100

    convert potentiometer to time Close #6

--- Physique/src/renderer/index.js
+++ Physique/src/renderer/index.js
@@ -7,15 +7,12 @@
 
 
 
-const { Board, GPS } = require("johnny-five");
+const { Board, GPS, Sensor } = require("johnny-five");
 
 const init = () => {
   getArduinoData();
   getLocation();
 }
-
-
-
 
 //Check if GPS board works. If not continue with hardcoded coordinates (set on Kortrijk)
 const checkGpsData = gpsData => {
@@ -27,6 +24,9 @@ const checkGpsData = gpsData => {
     getWeather(gpsData.lon, gpsData.lat);
   }
 }
+
+let timeByPot;
+let weatherData = [];
 
 const getArduinoData = () => {
   const board = new Board({
@@ -40,28 +40,39 @@ const getArduinoData = () => {
         tx: 10,
       }
     });
-  
-    // If latitude, longitude data log it.
-    // This will output zero until a valid
-    // GPS position is detected.
-    gps.on("data", position => {
-      const {altitude, latitude, longitude} = position;
-      console.log("GPS Position:");
-      console.log("  altitude   : ", altitude);
-      console.log("  latitude   : ", latitude);
-      console.log("  longitude  : ", longitude);
-      console.log("--------------------------------------");
-    });
+
     const gpsData = {
       lat: gps.latitude,
       long: gps.longitude
     }
+
     checkGpsData(gpsData);
+
+
+    const potentiometer = new Sensor("A3");
+
+    potentiometer.on("change", () => {
+      const {value, raw} = potentiometer;
+      //console.log(potentiometer.value)
+      timeByPot = showValue(potentiometer);
+      document.querySelector(".time").textContent = timeByPot;
+      showCurrentWeather(weatherData, timeByPot);
+    });
   });
 };
 
 
-
+const showCurrentWeather = (weatherData, timeByPot) => {
+  const hour = timeByPot.substring(0, 2);
+  const $temp = document.querySelector('.temp');
+  $temp.textContent = `${weatherData[0].temp}°C`
+  if (hour === weatherData[0].time) {
+    console.log(weatherData[0].time);
+    console.log("match");
+  } else {
+    console.log("no match");
+  }
+};
 
 
 // Fetch JSON file with weather
@@ -70,6 +81,7 @@ const getWeather = (lon, lat) => {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`)  
   .then(function(resp) { return resp.json() }) // Convert data to json
   .then(function(data) {
+    //convertWeatherData(data);
     convertWeatherData(data);
   })
   .catch(function() {
@@ -79,13 +91,13 @@ const getWeather = (lon, lat) => {
 
 
 
-let weatherData = [];
+
 
 
 
 // Convert JSON to data I need 
 const convertWeatherData = data => {
-  console.log(data);
+  //const time = document.querySelector(".time").textContent;
   for (let i = 0; i < 8; i++) {
     const item = data.list[i];
     const weatherObj = {
@@ -96,7 +108,8 @@ const convertWeatherData = data => {
     weatherData.push(weatherObj);
   }
   console.log(weatherData);
-  document.querySelector('.temp').textContent = `${weatherData[0].time}:00 --- ${weatherData[0].temp}°C`;
+  console.log(timeByPot);
+  //return weatherData;
 }
 
 
@@ -107,7 +120,38 @@ const convertTime = time => {
 }
 
 
+const convertInputToTime = input => {
+  const convert = input / 60;
+  const hour = convert.toString().split(".")[0];
+  const convertedHours = ("0" + hour).slice(-2);
+  const minutes = (parseFloat(convert.toString().split(".")[1]) * 60).toString().substring(0, 2);
+  const convertMinutes = ("0" + minutes).slice(-2);
+  return `${convertedHours}:${convertMinutes}`;
+}
 
+
+const getCurrentTime = () => {
+  const currentTime = new Date(Date.now());
+  const hours = currentTime.getHours() * 60;
+  const minutes = currentTime.getMinutes();
+  return hours + minutes;
+}
+
+
+
+const showValue = potentiometer => {
+  const currentTime = getCurrentTime(); //Tijd ophalen via code en dan omzetten naar minuten
+  const input = potentiometer.value * 1.40762463
+  const currentDay = 1440 - currentTime;
+  if (input <= currentDay) {
+    const fullTime = input + currentTime;
+    return convertInputToTime(fullTime);
+  } else {
+    const resetTime  = input - currentDay;
+    return convertInputToTime(resetTime);
+  }
+  
+}
 
 
 
