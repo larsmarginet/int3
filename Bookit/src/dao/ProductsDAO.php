c13605f149f059642b4acb870158d0cd89e0a5dc
larsmarginet <lars.marginet@student.howest.be>
2020-01-12 13:43:21 +0100

    overview all products Close #17

--- Bookit/src/dao/ProductsDAO.php
+++ Bookit/src/dao/ProductsDAO.php
@@ -5,7 +5,7 @@ require_once( __DIR__ . '/DAO.php');
 class ProductDAO extends DAO {
 
   public function selectAllProducts(){
-    $sql = "SELECT * FROM `products`";
+    $sql = "SELECT `products`.*, AVG(`reviews`.`score`) AS `averagescore`, COUNT(`reviews`.`score`) AS `countscore` FROM `products` LEFT JOIN `reviews` ON `products`.`id` = `reviews`.`product_id` GROUP BY `products`.`id`";
     $stmt = $this->pdo->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
