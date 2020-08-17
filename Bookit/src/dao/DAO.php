3baeef65a66906113be0e51b6d2468f62f60bab6
Lars Marginet <56624961+larsmarginet@users.noreply.github.com>
2020-08-17 12:27:39 +0200

    last commit

--- Bookit/src/dao/DAO.php
+++ Bookit/src/dao/DAO.php
@@ -11,10 +11,10 @@ class DAO {
 
 		if(empty(self::$sharedPDO)) {
 
-      $dbHost = getenv('PHP_DB_HOST') ?: "ID281906_int3.db.webhosting.be";
-      $dbName = getenv('PHP_DB_DATABASE') ?: "ID281906_int3";
-      $dbUser = getenv('PHP_DB_USERNAME') ?: "ID281906_int3";
-      $dbPass = getenv('PHP_DB_PASSWORD') ?: "databaseint3";
+      $dbHost = getenv('PHP_DB_HOST');
+      $dbName = getenv('PHP_DB_DATABASE');
+      $dbUser = getenv('PHP_DB_USERNAME');
+      $dbPass = getenv('PHP_DB_PASSWORD');
 
 			self::$sharedPDO = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPass);
 			self::$sharedPDO->exec("SET CHARACTER SET utf8");
